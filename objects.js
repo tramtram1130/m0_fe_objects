2c231cb372d78e9f6fa426008d23df0de66fecbf
Dana Truong <tramtramphotos@gmail.com>
2022-08-04 12:34:14 -0500

    Initial commit

--- objects.js
+++ objects.js
@@ -6,31 +6,33 @@
 // file in your Terminal.
 
 foods = {
-  apples: 23, 
-  grapes: 507, 
+  apples: 23,
+  grapes: 507,
   eggs: 48
 }
 
-// Write code that prints all of the 'keys' of the foods variable 
+// Write code that prints all of the 'keys' of the foods variable
 // you created above:
-// YOUR CODE HERE
+console.log(Object.keys(foods));
 
-// Write code that prints all of the 'values' of the foods variable 
+// Write code that prints all of the 'values' of the foods variable
 // you created above:
-// YOUR CODE HERE
+console.log(Object.values(foods));
 
-// Write code that prints the value of the second food of the foods variable 
+// Write code that prints the value of the second food of the foods variable
 // you created above:
-// YOUR CODE HERE
+console.log(foods.grapes);
 
-// Write code that adds a food to the foods object. 
+// Write code that adds a food to the foods object.
 // Then, print the updated object:
-// YOUR CODE HERE
+foods.dragonFruit = 10
+console.log(foods);
 
 
-#-------------------
+
+// -------------------
 // Part 2: Email
-#-------------------
+//-------------------
 
 
 // Think about all the pieces of information associated with one single email in your inbox.
@@ -39,21 +41,32 @@ foods = {
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
+var email = {
+  sender: 'Josh',
+  subject: 'Saturday Night Plans',
+  attachments: 4,
+  message: '"These are some good choices for dinner tonight. I have attached the menus below."',
+  additionalReceivers: ['Cassie', 'Marcial', 'Stasha']
+}
+
 // Write code that logs your email object to the terminal.
 
+console.log(email);
 
-// Write code that logs all of the 'keys' of the email object 
+// Write code that logs all of the 'keys' of the email object
 // you created above:
-// YOUR CODE HERE
 
-// Write code that logs all of the 'values' of the email object 
+console.log(Object.keys(email));
+
+// Write code that logs all of the 'values' of the email object
 // you created above:
-// YOUR CODE HERE
+
+console.log(Object.values(email));
 
 
-#-------------------
+//-------------------
 // Part 3: Many Emails - CHALLENGE!
-#-------------------
+//-------------------
 
 // LONG EXAMPLE:
 // Now that we've learned about Objects AND Arrays, we can combine them.
@@ -76,7 +89,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     imageSrc: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -90,12 +103,40 @@ console.log(posts);
 console.log(posts[0]);
 
 
-// The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-// Objects. Each of those Objects has 4 key-value pairs. This may LOOK 
+// The code snippet above shows an Array with 2 elements. Each element in the Array is a
+// Objects. Each of those Objects has 4 key-value pairs. This may LOOK
 // a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 // good to have some exposure before going into Mod 1.
 
 
-// YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
+// YOU DO: Create an array of at least 3 EMAIL Objects, using the same
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+
+emails = [
+  {
+    sender: 'Josh',
+    subject: 'Saturday Night Plans',
+    attachments: 4,
+    message: '"These are some good choices for dinner tonight. I have attached the menus below."',
+    additionalReceivers: ['Cassie', 'Marcial', 'Stasha']
+  },
+  {
+    sender: 'Stasha',
+    subject: 'Movie tickets',
+    attachment: 1,
+    message: '"I sent you a Venmo request!"',
+    additionalReceivers: []
+  },
+  {
+    sender: 'Prince of Nigeria',
+    subject: 'Oppurtunities to make MILLIONS!',
+    attachment: 2,
+    message: '"I am in need of assistance and will greatly repay you! Please go to the link below."',
+    additionalReceivers: ['random1', 'random2', 'random3', 'random4', 'random5']
+  }
+];
+
+console.log(emails);
+console.log("==========================");
+console.log(emails[2]);
--- selfEval.md
+++ selfEval.md
@@ -1,5 +1,13 @@
 # Self Evaluation
 
 - What did you struggle with in this section?
+
+Sometimes I catch myself typing too fast and end up mistyping the syntax so I just need to take my time.
+
 - How is note-taking going? What are you finding helpful? When do you find yourself taking the most helpful notes?
+
+Note taking is going and it's been helping me a lot. I notice that I retain concepts and obscure rules better when I write it down.
+
 - What are you noticing that you lean into - Googling, or trying to solve a problem in code on your own first?
+
+I attempt to solve it myself first before Googling.
